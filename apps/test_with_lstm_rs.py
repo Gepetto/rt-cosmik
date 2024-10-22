910ffc2aaeaed4c69a5b90851d99f8a870f097fa
msabbah <msabbah@laas.fr>
2024-10-22 06:22:38 +0000

    setup to test triangulation + rviz visu

--- apps/test_with_lstm_rs.py
+++ apps/test_with_lstm_rs.py
@@ -198,12 +198,12 @@ def main():
     args = parse_args()
     np.set_printoptions(precision=4, suppress=True)
 
-    csv_file_path = '/home/kahina/mmdeploy-1.0.0-linux-x86_64-cxx11abi-cuda11.3/example/python/output0/keypoints_3d_positions_test.csv'
-    csv2_file_path = '/home/kahina/mmdeploy-1.0.0-linux-x86_64-cxx11abi-cuda11.3/example/python/output0/q.csv'
+    csv_file_path = os.path.join(parent_directory,'/output/keypoints_3d_positions.csv')
+    csv2_file_path = os.path.join(parent_directory,'/output/q.csv')
 
-    K1, D1 = load_cam_params("/home/kahina/mmdeploy-1.0.0-linux-x86_64-cxx11abi-cuda11.3/cam_calibration/cam_params/c1_params_color_1_1.yml")
-    K2, D2 = load_cam_params("/home/kahina/mmdeploy-1.0.0-linux-x86_64-cxx11abi-cuda11.3/cam_calibration/cam_params/c2_params_color_1_1.yml")
-    R,T = load_cam_to_cam_params("/home/kahina/mmdeploy-1.0.0-linux-x86_64-cxx11abi-cuda11.3/cam_calibration/cam_params/c1_to_c2_params_color_1_1.yml")
+    K1, D1 = load_cam_params(os.path.join(parent_directory,"/cams_calibration/cam_params/c1_params_color_test_test.yml"))
+    K2, D2 = load_cam_params(os.path.join(parent_directory,"/cams_calibration/cam_params/c2_params_color_test_test.yml"))
+    R,T = load_cam_to_cam_params(os.path.join(parent_directory,"/cams_calibration/cam_params/c1_to_c2_params_color_test_test.yml"))
 
     dict_cam = {
         "cam1": {
