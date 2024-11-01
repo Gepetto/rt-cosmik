c694e649f085efeed5b81a71b053c964c1266fdc
msabbah06 <msabbah@laas.fr>
2024-11-01 08:35:48 +0000

    good visu in rviz

--- apps/test_ik_rviz.py
+++ apps/test_ik_rviz.py
@@ -15,10 +15,11 @@ import numpy as np
 from utils.model_utils import build_model_challenge
 from utils.ik_utils import RT_IK
 from utils.viz_utils import place, Rquat
-from utils.ros_utils import publish_kinematics
+from utils.ros_utils import publish_kinematics, publish_augmented_markers
 import time
 import rospy
 from sensor_msgs.msg import JointState
+from visualization_msgs.msg import MarkerArray
 import tf2_ros
 
 data_markers = pd.read_csv(os.path.join(rt_cosmik_path,'output/augmented_markers_positions.csv'))
@@ -48,8 +49,19 @@ print("Time to build the model: ", t2-t1)
 
 dof_names=['middle_lumbar_Z', 'middle_lumbar_Y', 'right_shoulder_Z', 'right_shoulder_X', 'right_shoulder_Y', 'right_elbow_Z', 'right_elbow_Y', 'left_shoulder_Z', 'left_shoulder_X', 'left_shoulder_Y', 'left_elbow_Z', 'left_elbow_Y', 'right_hip_Z', 'right_hip_X', 'right_hip_Y', 'right_knee_Z', 'right_ankle_Z','left_hip_Z', 'left_hip_X', 'left_hip_Y', 'left_knee_Z', 'left_ankle_Z'] 
 
+marker_names = ['r.ASIS_study','L.ASIS_study','r.PSIS_study','L.PSIS_study','r_knee_study',
+           'r_mknee_study','r_ankle_study','r_mankle_study','r_toe_study','r_5meta_study',
+           'r_calc_study','L_knee_study','L_mknee_study','L_ankle_study','L_mankle_study',
+           'L_toe_study','L_calc_study','L_5meta_study','r_shoulder_study','L_shoulder_study',
+           'C7_study','r_thigh1_study','r_thigh2_study','r_thigh3_study','L_thigh1_study',
+           'L_thigh2_study','L_thigh3_study','r_sh1_study','r_sh2_study','r_sh3_study',
+           'L_sh1_study','L_sh2_study','L_sh3_study','RHJC_study','LHJC_study','r_lelbow_study',
+           'r_melbow_study','r_lwrist_study','r_mwrist_study','L_lelbow_study','L_melbow_study',
+           'L_lwrist_study','L_mwrist_study']
+
 rospy.init_node('human_rt_ik', anonymous=True)
 pub = rospy.Publisher('/human_RT_joint_angles', JointState, queue_size=10)
+augmented_markers_pub = rospy.Publisher('/markers_pose', MarkerArray, queue_size=10)
 
 ### IK init 
 q = pin.neutral(human_model) # init pos
@@ -84,7 +96,6 @@ ik_class._q0=q
 br = tf2_ros.TransformBroadcaster()
 publish_kinematics(q, br,pub,dof_names)
 
-input()
 
 for ii in range(100,len(result_markers)): 
     lstm_dict = result_markers[ii]
@@ -92,6 +103,9 @@ for ii in range(100,len(result_markers)):
     q = ik_class.solve_ik_sample_quadprog() 
     ik_class._q0 = q 
 
+    augmented_markers = list(lstm_dict.values())
+
+    publish_augmented_markers(augmented_markers, augmented_markers_pub, marker_names)
     publish_kinematics(q, br,pub,dof_names)
 
     input()
