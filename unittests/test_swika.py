c9a08a22d6f31d7c75328062e349d739d63ddb09
Maxime Sabbah <msabbah@laas.fr>
2024-11-09 13:38:12 +0000

    swika working, must run on the trajectory

--- unittests/test_swika.py
+++ unittests/test_swika.py
@@ -13,14 +13,23 @@ import pinocchio as pin
 from pinocchio.visualize import GepettoVisualizer
 import numpy as np
 from utils.model_utils import build_model_challenge
-from utils.ik_utils import RT_SWIKA
+from utils.ik_utils import RT_SWIKA, RT_IK
 from utils.viz_utils import place, Rquat
 import time
 from collections import deque
+import csv
 
 ii=0
+q_csv_file_path = os.path.join(rt_cosmik_path,'output/q_swika.csv')
 
-deque_lstm_dict = deque(maxlen=30)
+dof_names=['middle_lumbar_Z', 'middle_lumbar_Y', 'right_shoulder_Z', 'right_shoulder_X', 'right_shoulder_Y', 'right_elbow_Z', 'right_elbow_Y', 'left_shoulder_Z', 'left_shoulder_X', 'left_shoulder_Y', 'left_elbow_Z', 'left_elbow_Y', 'right_hip_Z', 'right_hip_X', 'right_hip_Y', 'right_knee_Z', 'right_ankle_Z','left_hip_Z', 'left_hip_X', 'left_hip_Y', 'left_knee_Z', 'left_ankle_Z'] 
+
+with open(q_csv_file_path, mode='w', newline='') as file:
+    csv_writer = csv.writer(file)
+    # Write the header row
+    csv_writer.writerow(['FF_X', 'FF_Y', 'FF_Z', 'FF_QUAT_X', 'FF_QUAT_Y', 'FF_QUAT_Z','FF_QUAT_W' ] + dof_names)
+
+deque_lstm_dict = deque(maxlen=10)
     
 data_markers = pd.read_csv(os.path.join(rt_cosmik_path,'output/augmented_markers_positions.csv'))
 data_keypoints = pd.read_csv(os.path.join(rt_cosmik_path,'output/keypoints_3d_positions.csv'))
@@ -87,45 +96,34 @@ marker_names = ['r.ASIS_study','L.ASIS_study','r.PSIS_study','L.PSIS_study','r_k
 ### IK init 
 dt = 1/40
 T=10
-keys_to_track_list = ['C7_study',
-                        'r.ASIS_study', 'L.ASIS_study', 
-                        'r.PSIS_study', 'L.PSIS_study', 
-                        
-                        'r_shoulder_study',
-                        'r_lelbow_study', 'r_melbow_study',
-                        'r_lwrist_study', 'r_mwrist_study',
-                        'r_ankle_study', 'r_mankle_study',
-                        'r_toe_study','r_5meta_study', 'r_calc_study',
-                        'r_knee_study', 'r_mknee_study',
-                        'r_thigh1_study', 'r_thigh2_study', 'r_thigh3_study',
-                        'r_sh1_study', 'r_sh2_study', 'r_sh3_study',
-                        
-                        'L_shoulder_study', 
-                        'L_lelbow_study', 'L_melbow_study',
-                        'L_lwrist_study','L_mwrist_study',
-                        'L_ankle_study', 'L_mankle_study', 
-                        'L_toe_study','L_5meta_study', 'L_calc_study',
-                        'L_knee_study', 'L_mknee_study',
-                        'L_thigh1_study', 'L_thigh2_study', 'L_thigh3_study',
-                        'L_sh1_study', 'L_sh2_study', 'L_sh3_study']
+
+keys_to_track_list = marker_names
 
 x_list = []
-q_list = []
 
-x0 = np.zeros(human_model.nv + human_model.nv)
-q0 = np.zeros(human_model.nq)
+x0 = np.zeros(human_model.nq + human_model.nv)
+x0[6] = 1
+
 for ii in range(T):
     x_list.append(x0)
-    q_list.append(q0)
     deque_lstm_dict.append(lstm_dict)
 
 ### IK calculations
-ik_class = RT_SWIKA(human_model, deque_lstm_dict, x_list, q_list, keys_to_track_list, T, dt)
-sol, new_x_list, new_q_list = ik_class.solve_swika_casadi()
+ik_class = RT_SWIKA(human_model, deque_lstm_dict, x_list, keys_to_track_list, T, dt)
+# ik_class=RT_IK(human_model, lstm_dict, x0[:human_model.nq], keys_to_track_list, dt)
+sol, new_x_list = ik_class.solve_swika_casadi()
+q = new_x_list[-1][:human_model.nq]
+# q = ik_class.solve_ik_sample_casadi()
 
-viz.display(new_q_list[0])
+viz.display(q)
+# Saving kinematics
+with open(q_csv_file_path, mode='a', newline='') as file:
+    csv_writer = csv.writer(file)
+    # Write to CSV
+    csv_writer.writerow(q.tolist())
 
-pin.forwardKinematics(human_model, human_data, new_q_list[0])
+
+pin.forwardKinematics(human_model, human_data,q)
 pin.updateFramePlacements(human_model, human_data)
 for dof in dof_names:
     place(viz,'world/'+dof,human_data.oMi[human_model.getJointId(dof)])
@@ -143,7 +141,6 @@ for marker in result_markers[ii].keys():
     M = human_data.oMf[human_model.getFrameId(marker)]
     place(viz,'world/estimated'+marker,M)
 
-input()
 
 while ii < len(result_markers):
     ii+=1
@@ -153,14 +150,20 @@ while ii < len(result_markers):
     deque_lstm_dict.append(lstm_dict)
 
     ik_class._x_list = new_x_list
-    ik_class._q_list = new_q_list
     ik_class._deque_dict_m = deque_lstm_dict
 
-    sol, new_x_list, new_q_list = ik_class.solve_swika_casadi()
+    sol, new_x_list = ik_class.solve_swika_casadi()
 
-    viz.display(new_q_list[-1])
+    q = new_x_list[-1][:human_model.nq]
 
-    pin.forwardKinematics(human_model, human_data, new_q_list[-1])
+    viz.display(q)
+    # Saving kinematics
+    with open(q_csv_file_path, mode='a', newline='') as file:
+        csv_writer = csv.writer(file)
+        # Write to CSV
+        csv_writer.writerow(q.tolist())
+
+    pin.forwardKinematics(human_model, human_data, q)
     pin.updateFramePlacements(human_model, human_data)
     for dof in dof_names:
         place(viz,'world/'+dof,human_data.oMi[human_model.getJointId(dof)])
