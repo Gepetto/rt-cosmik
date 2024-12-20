b535482323e3f4b3f311b57489cbd6c4e6be0822
msabbah <msabbah@laas.fr>
2024-12-20 13:25:32 +0000

    typo on csv of q

--- apps/rt_cosmik_rgbcams_wb_rviz.py
+++ apps/rt_cosmik_rgbcams_wb_rviz.py
@@ -112,7 +112,7 @@ def main():
     ### Initialize CSV files
     init_csv(keypoints_csv_file_path,['Frame', 'Time','Keypoint', 'X', 'Y', 'Z'])
     init_csv(augmented_csv_file_path,['Frame', 'Time','Marker', 'X', 'Y', 'Z'])
-    init_csv(q_csv_file_path,['Frame','Time','q0', 'q1','q2','q3','q4'])
+    init_csv(q_csv_file_path,['Frame','Time','q0', 'q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21','q22'])
 
     ### Initialize ROS node 
     rospy.init_node('human_rt_ik', anonymous=True)
