df95e1f640c8aad1bea0bf74e30195c6ccebf9d6
msabbah <msabbah@laas.fr>
2025-01-09 17:16:11 +0000

    no visu

--- handover/get_traj_for_irl.py
+++ handover/get_traj_for_irl.py
@@ -210,14 +210,14 @@ def main():
                 keypoints, bboxes, _ = results
                 keypoints = (keypoints[..., :2] ).astype(float)
                 
-                if not visualize(
-                        frame,
-                        results,
-                        args.output_dir,
-                        idx,
-                        frame_idx + idx,
-                        skeleton_type=args.skeleton):
-                    break
+                # if not visualize(
+                #         frame,
+                #         results,
+                #         args.output_dir,
+                #         idx,
+                #         frame_idx + idx,
+                #         skeleton_type=args.skeleton):
+                #     break
 
                 if keypoints.size == 0 or keypoints.flatten().shape != (52,):
                     pass
