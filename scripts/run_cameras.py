4bba8098067748ecf0afef65fe8f600eb5b48d73
msabbah06 <msabbah@laas.fr>
2025-03-05 09:49:26 +0100

    fix timestamps

--- scripts/run_cameras.py
+++ scripts/run_cameras.py
@@ -33,6 +33,7 @@ def main():
     display = DisplayConsumer(
         camera_buffers=camera_buffers,
         camera_locks=camera_locks,
+        camera_timestamps=camera_timestamps,
         stop_event=stop_event,
         frame_shape=FRAME_SHAPE,
         num_cameras=NUM_CAMERAS
