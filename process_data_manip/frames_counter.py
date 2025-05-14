1865bcc0e7bb036a9bf6b02a2c40b27c19699a3b
root <kchalabi@laas.fr>
2025-05-14 16:06:11 +0000

    Restore deleted folder from previous commit

--- process_data_manip/frames_counter.py
+++ process_data_manip/frames_counter.py
@@ -16,3 +16,22 @@ def count_frames(video_path):
 # Example usage
 video_path = '/root/workspace/ros_ws/src/rt-cosmik/output/trial1/welding/camera_0.mp4'
 count_frames(video_path)
+
+import cv2
+
+def count_frames(video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
+
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+
+    print(f"Total number of frames: {total_frames}")
+    return total_frames
+
+# Example usage
+video_path = '/root/workspace/ros_ws/src/rt-cosmik/output/test/camera_0.mp4'
+count_frames(video_path)
