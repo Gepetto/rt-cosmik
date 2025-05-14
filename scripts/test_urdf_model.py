fix joint placement in scaling function

scripts/test_urdf_model.py
@@ -20,7 +20,7 @@ from src.rtcosmik.human_model.model_utils import get_segment_length
 
 #read mks data
 no_trial = "trial_2"
-task = "trial_upper"
+task = "trial_static"
 path_to_csv = f"/root/workspace/ros_ws/src/rt-cosmik/output/{no_trial}/{task}/mks_pose.csv"
 ###########################################################################################for cosmik data 
 # path_to_kpt = f"/root/workspace/ros_ws/src/rt-cosmik/output/{no_trial}/{task}/keypoints.csv"
@@ -56,7 +56,7 @@ human_visual_model = human.visual_model
 human_model = scale_human_model(human_model, start_sample_dict,with_hand=True,gender='male',subject_height=1.81)
 print(human_model.nq)
 
-# human_model= mks_registration(human_model,start_sample_dict, with_hand=False, gender='male',subject_height=1.81)
+human_model= mks_registration(human_model,start_sample_dict, with_hand=True, gender='male',subject_height=1.81)
 
 human_data = pin.Data(human_model)
 # human_collision_model = human.collision_model
@@ -74,16 +74,16 @@ for frame in human_model.frames.tolist():
 
 # get_segment_length(start_sample_dict)
 # measured frames
-seg_frames = construct_segments_frames(start_sample_dict)
-add_frames(viz,seg_frames,"meas", 0.008, 0.08)
-for seg_name, M in seg_frames.items():
+# seg_frames = construct_segments_frames(start_sample_dict)
+# add_frames(viz,seg_frames,"meas", 0.008, 0.08)
+# for seg_name, M in seg_frames.items():
         
-        frame_name = f'world/{seg_name+"_meas"}'
-        frame_se3 = pin.SE3(M[:3,:3], np.matrix([M[0,3],M[1,3],M[2,3]]).T)
-        place(viz, frame_name, frame_se3)
-# # #display markers
-for key in start_sample_dict.keys():
-    place(viz, 'world/'+key, pin.SE3(np.eye(3), np.array([start_sample_dict[key][0],start_sample_dict[key][1],start_sample_dict[key][2]])))
+#         frame_name = f'world/{seg_name+"_meas"}'
+#         frame_se3 = pin.SE3(M[:3,:3], np.matrix([M[0,3],M[1,3],M[2,3]]).T)
+#         place(viz, frame_name, frame_se3)
+# # # #display markers
+# for key in start_sample_dict.keys():
+#     place(viz, 'world/'+key, pin.SE3(np.eye(3), np.array([start_sample_dict[key][0],start_sample_dict[key][1],start_sample_dict[key][2]])))
 
 q =pin.neutral(human_model)
 

src/rtcosmik/human_model/urdf_model.py
@@ -75,28 +75,28 @@ def scale_human_model(model, mks_positions, with_hand=True,gender='male',subject
     local_segments_positions = get_local_segments_positions(sgts_poses,with_hand=True)
 
     model.jointPlacements[model.getJointId('left_hip_Z')].translation=local_segments_positions['thighL']
-    model.jointPlacements[model.getJointId('left_knee')].translation=local_segments_positions['shankL']
-    model.jointPlacements[model.getJointId('left_ankle_Z')].translation=local_segments_positions['footL']
+    model.jointPlacements[model.getJointId('left_knee')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['shankL']),0)
+    model.jointPlacements[model.getJointId('left_ankle_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['footL']),0)
 
     model.jointPlacements[model.getJointId('middle_lumbar_Z')].translation=np.array([0,0,0])
-    model.jointPlacements[model.getJointId('middle_thoracic_Z')].translation=local_segments_positions['thorax']
+    model.jointPlacements[model.getJointId('middle_thoracic_Z')].translation=col_vector_3D(0, np.linalg.norm(local_segments_positions['thorax']),0)
 
-    model.jointPlacements[model.getJointId('left_clavicle_joint_X')].translation=local_segments_positions['torso']
-    model.jointPlacements[model.getJointId('right_clavicle_joint_X')].translation=local_segments_positions['torso']
-    model.jointPlacements[model.getJointId('middle_cervical_Z')].translation=local_segments_positions['torso']
+    model.jointPlacements[model.getJointId('left_clavicle_joint_X')].translation=col_vector_3D(0, np.linalg.norm(local_segments_positions['torso']),0)
+    model.jointPlacements[model.getJointId('right_clavicle_joint_X')].translation=col_vector_3D(0, np.linalg.norm(local_segments_positions['torso']),0)
+    model.jointPlacements[model.getJointId('middle_cervical_Z')].translation=col_vector_3D(0, np.linalg.norm(local_segments_positions['torso']),0)
    
     model.jointPlacements[model.getJointId('left_shoulder_Z')].translation=local_segments_positions['upperarmL']
-    model.jointPlacements[model.getJointId('left_elbow_Z')].translation=local_segments_positions['lowerarmL']
+    model.jointPlacements[model.getJointId('left_elbow_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['lowerarmL']),0)
 
     model.jointPlacements[model.getJointId('right_shoulder_Z')].translation=local_segments_positions['upperarmR']
-    model.jointPlacements[model.getJointId('right_elbow_Z')].translation=local_segments_positions['lowerarmR']
+    model.jointPlacements[model.getJointId('right_elbow_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['lowerarmR']),0)
     model.jointPlacements[model.getJointId('right_hip_Z')].translation=local_segments_positions['thighR']
-    model.jointPlacements[model.getJointId('right_knee')].translation=local_segments_positions['shankR']
-    model.jointPlacements[model.getJointId('right_ankle_Z')].translation=local_segments_positions['footR']
+    model.jointPlacements[model.getJointId('right_knee')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['shankR']),0)
+    model.jointPlacements[model.getJointId('right_ankle_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['footR']),0)
 
     if with_hand:
-        model.jointPlacements[model.getJointId('left_wrist_Z')].translation=local_segments_positions['handL']
-        model.jointPlacements[model.getJointId('right_wrist_Z')].translation=local_segments_positions['handR']
+        model.jointPlacements[model.getJointId('left_wrist_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['handL']),0)
+        model.jointPlacements[model.getJointId('right_wrist_Z')].translation=col_vector_3D(0, -np.linalg.norm(local_segments_positions['handR']),0)
     return model
 
 def mks_registration(model,mks_positions, with_hand=True, gender='male',subject_height=1.8):