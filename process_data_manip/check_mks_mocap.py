3427aac1ca7fbfbcffaf14c071b90567f33bb25d
root <kchalabi@laas.fr>
2025-05-15 16:24:11 +0000

    adapt head pose to new markers

--- process_data_manip/check_mks_mocap.py
+++ process_data_manip/check_mks_mocap.py
@@ -10,19 +10,20 @@ from pinocchio.visualize import GepettoVisualizer
 from src.rtcosmik.utils.read_write_utils import parse_marker_csv,udp_csv_to_dataframe,read_mks_data,marker_data_to_dataframe
 from viz_utils import place
 
-no_trial = "trial3"
-task = "upper"
+no_trial = "Nicolas"
+task = "static"
 path_to_csv = f"/root/workspace/ros_ws/src/rt-cosmik/output/{no_trial}/{task}/mks_data.csv"
 df_raw = pd.read_csv(path_to_csv)
 
-marker_mocap_names = ['r.ASIS_study','L.ASIS_study','r.PSIS_study','L.PSIS_study',
+marker_mocap_names = ['r.PSIS_study','L.PSIS_study','r.ASIS_study','L.ASIS_study',
              'TV8','TV12','SJN','STRN','C7_study','r_shoulder_study','L_shoulder_study',
-             'LBHD','RBHD','LFHD','RFHD',
-             'L_lelbow_study','L_melbow_study','LUArm','L_lwrist_study','L_mwrist_study','LForearm','LHand1','LHand2','LHL2','LHM5',
-             'r_lelbow_study','r_melbow_study','RUArm','r_lwrist_study','r_mwrist_study','RForearm','RHand1','RHand2','RHL2','RHM5',
+             'BHD','RHD','LHD','FHD',
+             'L_lelbow_study','L_melbow_study','LUArm','L_lwrist_study','L_mwrist_study','LForearm','LHand','LHL2','LHM5',
+             'r_lelbow_study','r_melbow_study','RUArm','r_lwrist_study','r_mwrist_study','RForearm','RHand','RHL2','RHM5',
              'L_thigh1_study','L_knee_study','L_mknee_study','L_sh1_study','L_ankle_study','L_mankle_study','L_calc_study','L_5meta_study','L_toe_study',
              'r_thigh1_study','r_knee_study','r_mknee_study','r_sh1_study',
-             'r_ankle_study','r_mankle_study','r_calc_study','r_5meta_study','r_toe_study']
+             'r_ankle_study','r_mankle_study','r_calc_study','r_5meta_study','r_toe_study',
+             'r_pelvis', 'l_pelvis']
 
 mks_names = marker_mocap_names
 # df_wide = marker_data_to_dataframe(df_raw,mks_names)
