4447bb22e67a7dec428d57bc83961e4033a65674
Nicocodu32 <nicolas.gouget@wanadoo.fr>
2025-07-08 11:42:33 +0200

    run_cosmik_fps_max to test and debug

--- process_data_manip/save_rmse_grid.py
+++ process_data_manip/save_rmse_grid.py
@@ -60,13 +60,24 @@ for subject in os.listdir(data_path):
     list_of_trials = os.listdir(os.path.join(data_path, subject))
     list_of_trials = [trial for trial in list_of_trials if trial != "cosmik_2cams" and trial != "output_2d" and trial != "results"]
     multi_cols = pd.MultiIndex.from_product([list_of_trials, ['rmse_deg', 'corr']])
-    metrics_par_dof_over_trials = pd.DataFrame(index=[dof for dof in dofs if dof not in excluded_dofs].append(["mean", "std"]), columns=multi_cols)
+    metrics_par_dof_over_trials = pd.DataFrame(index=['Lhip_flex_ext', 'Lhip_abd_add','Lhip_int_ext_rot','Lknee_flex_ext','Lankle_flex_ext','Lankle_abd_add',
+                          'Lumbar_flex_ext', 'Lumbar_lateral_flex',
+                          'thoracic_flex_ext','thoracic_lateral_flex','thoracic_rot_int_ext',
+                          'Lcalvicule_x',
+                          'Lshoulder_flex_ext','Lshoulder_abd_add', 'Lshoulder_int_ext_rot','Lelbow_flex_ext','Lelbow_pron_supi',
+                          'Cervical_flex_ext', 'Cervical_lat_bend', 'Cervical_int_ext_rot',
+                          'rcalvicule_x',
+                          'Rshoulder_flex_ext', 'Rshoulder_abd_add', 'Rshoulder_int_ext_rot','Relbow_flex_ext', 'Relbow_pron_supi',
+                          'Rhip_flex_ext','Rhip_abd_add','Rhip_int_ext_rot',
+                          'Rknee_flex_ext','Rankle_flex_ext', 'Rankle_abd_add', "mean", "std"], columns=multi_cols)
+    print([dof for dof in dofs if dof not in excluded_dofs].append(["mean", "std"]))
+    print(metrics_par_dof_over_trials.index)
     subject_path = os.path.join(data_path, subject)
-    cosmik_2cams_path = os.path.join(data_path, subject, "cosmik_2cams")
+    # cosmik_2cams_path = os.path.join(data_path, subject, "cosmik_2cams")
     for trial in list_of_trials:
         trial_path = os.path.join(subject_path, trial)
-        cosmik_2cams_trial_path = os.path.join(cosmik_2cams_path, trial)
-        path_cosmik = os.path.join(cosmik_2cams_trial_path, "q_cosmik_ipopt_2.csv")
+        # cosmik_2cams_trial_path = os.path.join(cosmik_2cams_path, trial)
+        path_cosmik = os.path.join(trial_path, "q_cosmik_ipopt_4.csv")
         path_mocap = os.path.join(trial_path, "q_mocap_ipopt.csv")
 
         q_cosmik = pd.read_csv(path_cosmik).iloc[:, 7:]
