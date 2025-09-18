bf042dce479d8c462dbca4c39fb6748eb3a455cd
Nicocodu32 <nicolas.gouget@wanadoo.fr>
2025-09-18 09:40:04 +0200

    add converter for jcp mocap in mm

--- scripts/automated_testing.sh
+++ scripts/automated_testing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+read -p "test over mocap (T/F)? " test_over_mocap
 read -p "upper id ? " upper_id
 read -p "lower id ? " lower_id
 read -p "test subjects ? " test_subjects
@@ -10,7 +11,7 @@ read -p "Visualize (T/F)? " visualize
 python convert_tf2onnx_only_HPE.py --body-part upper --model-id $upper_id
 python convert_tf2onnx_only_HPE.py --body-part lower --model-id $lower_id
 
-python run_marker_augmenter_only_HPE.py --upper-model-id $upper_id --lower-model-id $lower_id --test-subjects $test_subjects #--excluded-trials $excluded_trials
+python run_marker_augmenter_only_HPE.py --test-over-mocap $test_over_mocap --upper-model-id $upper_id --lower-model-id $lower_id --test-subjects $test_subjects #--excluded-trials $excluded_trials
 
-python ../process_data_manip/z_check_multiple_mks_gp_v_only_HPE.py --upper-model-id $upper_id --lower-model-id $lower_id \
+python ../process_data_manip/z_check_multiple_mks_gp_v_only_HPE.py --test-over-mocap $test_over_mocap --upper-model-id $upper_id --lower-model-id $lower_id \
  --test-subjects $test_subjects --visualize $visualize #--excluded-trials $excluded_trials
