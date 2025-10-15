330cd15fc967571613871197b813120bb51a4ea0
root <kchalabi@laas.fr>
2025-10-15 13:14:10 +0000

    minor modif units

--- scripts/fine_tune_lstm_dynamic_only_mocap100hz.py
+++ scripts/fine_tune_lstm_dynamic_only_mocap100hz.py
@@ -203,7 +203,7 @@ def trial_to_windows(
     df_in = pd.DataFrame(arr_in, columns=cols_in)
     df_gt = pd.DataFrame(arr_gt, columns=cols_gt)
 
-    k_list, _ = read_mks_data(df_in, converter=1000)        # includes 'midHip'
+    k_list, _ = read_mks_data(df_in, converter=1.0)        # includes 'midHip'
     m_list, _ = read_mks_data(df_gt, converter=1000)
 
     # Build arrays
